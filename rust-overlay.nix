49449694489ff25f85ac7e3d41ee81df7f03a059
Anders Christiansen Sørby <anders@sorby.xyz>
2021-06-11 13:53:11 +0200

    Fix lib reference

--- rust-overlay.nix
+++ rust-overlay.nix
@@ -293,7 +293,7 @@ let
             # And get a fully working Rust compiler, with the stdenv linker.
             propagatedBuildInputs = [ stdenv.cc ];
 
-            meta.platforms = lib.platforms.all;
+            meta.platforms = super.lib.platforms.all;
           }
       ) { extensions = []; targets = []; targetExtensions = []; }
     );
